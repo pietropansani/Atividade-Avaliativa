343c3ac307611cf6cff757cc4d6dad9849f6d243
Pietro Pansani <pietropansani038@gmail.com>
2026-06-10 10:22:29 -0300

    Validação do Peso

--- Projeto-EcoSort.py
+++ Projeto-EcoSort.py
@@ -7,3 +7,12 @@ print("=== SISTEMA ECOSORT - PROCESSAMENTO DE LOTE ===")
 
 for i in range(TOTAL_PACOTES):
     print(f"\n--- Transação do Pacote {i + 1} ---")
+while True:
+        try:
+            peso = float(input(f"Digite o peso do pacote {i + 1} (em kg): "))
+            if peso > 0:
+                break  # Dado correto, sai do laço de validação
+            else:
+                print("[ERRO] O peso não pode ser negativo ou zero. Tente novamente.")
+        except ValueError:
+            print("[ERRO] Entrada inválida. Por favor, digite um número.")
